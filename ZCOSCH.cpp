d8b3ea19583d2889ca4552b8f54ca4d6da513877
Arpan Kumar Gorai <82395272+Ethan-Datsun@users.noreply.github.com>
2021-11-09 23:37:46 +0530

    Add files via upload

---
+++ ZCOSCH.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int r;
+    cin>>r;
+    if(r>=1 && r<=50)
+        cout<<"100\n";
+	else if(r>50 && r<=100)
+	    cout<<"50\n";
+	else
+	    cout<<"0\n";
+	return 0;
+}
